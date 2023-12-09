1f8a7199dda65e861c078e77df10e57665a7d9b2
doit132 <117004316+doit132@users.noreply.github.com>
2023-12-09 09:56:40 +0800

    增加右修饰键的释放, 例如: RCtrl, RShift, RAlt

--- projects/EscModKey/src/EscModKey_interception.cpp
+++ projects/EscModKey/src/EscModKey_interception.cpp
@@ -10,69 +10,80 @@
 enum ScanCode {
 	SCANCODE_LWIN = 0x5B,
 	SCANCODE_LALT = 0x38,
+	SCANCODE_RALT = 0x138,
 	SCANCODE_LSHIFT = 0x2A,
-	SCANCODE_LCTRL = 0x1D
+	SCANCODE_RSHIFT = 0x136,
+	SCANCODE_LCTRL = 0x1D,
+	SCANCODE_RCTRL = 0x11D
 };
 
-enum VirtCode { VIRTCODE_LWIN = 91, VIRTCODE_LALT = 18, VIRTCODE_LSHIFT = 16, VIRTCODE_LCTRL = 17 };
+enum VirtCode {
+	VIRTCODE_LWIN = 91,
+	VIRTCODE_LALT = 18,
+	VIRTCODE_RALT = 165,
+	VIRTCODE_LSHIFT = 16,
+	VIRTCODE_RSHIFT = 161,
+	VIRTCODE_LCTRL = 17,
+	VIRTCODE_RCTRL = 163
+};
 
 enum PhyKeyState { PHY_KEY_DOWN = false, PHY_KEY_UP = true };
 
 // 全局变量，用于线程同步
 HANDLE g_ThreadHandle = NULL;
-HANDLE g_ThreadHandle_LAlt = NULL;
-HANDLE g_ThreadHandle_LShift = NULL;
-HANDLE g_ThreadHandle_LCtrl = NULL;
+HANDLE g_ThreadHandle_Alt = NULL;
+HANDLE g_ThreadHandle_Shift = NULL;
+HANDLE g_ThreadHandle_Ctrl = NULL;
 HANDLE g_ThreadHandle_LWin = NULL;
 
 BOOL g_ExitThread = FALSE;
 
-bool phy_LAlt_pressed = PHY_KEY_UP;
-bool phy_LCtrl_pressed = PHY_KEY_UP;
-bool phy_LShift_pressed = PHY_KEY_UP;
+bool phy_Alt_pressed = PHY_KEY_UP;
+bool phy_Ctrl_pressed = PHY_KEY_UP;
+bool phy_Shift_pressed = PHY_KEY_UP;
 bool phy_LWin_pressed = PHY_KEY_UP;
 
 // 全局变量，定时器ID
-UINT_PTR g_TimerId_LAlt = 0;
-UINT_PTR g_TimerId_LShift = 0;
-UINT_PTR g_TimerId_LCtrl = 0;
+UINT_PTR g_TimerId_Alt = 0;
+UINT_PTR g_TimerId_Shift = 0;
+UINT_PTR g_TimerId_Ctrl = 0;
 UINT_PTR g_TimerId_LWin = 0;
 
 // ### 定时器回调函数 begin
-VOID CALLBACK TimerCallback_LCtrl(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+VOID CALLBACK TimerCallback_Ctrl(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
 	(void)dwTime;
 	(void)idEvent;
 	(void)hwnd;
 	(void)uMsg;
 	// 停止定时器
-	KillTimer(NULL, g_TimerId_LCtrl);
-	g_TimerId_LCtrl = 0;
-	phy_LCtrl_pressed = PHY_KEY_DOWN;
+	KillTimer(NULL, g_TimerId_Ctrl);
+	g_TimerId_Ctrl = 0;
+	phy_Ctrl_pressed = PHY_KEY_DOWN;
 }
 
-VOID CALLBACK TimerCallback_LShift(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+VOID CALLBACK TimerCallback_Shift(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
 	(void)dwTime;
 	(void)idEvent;
 	(void)hwnd;
 	(void)uMsg;
 	// 停止定时器
-	KillTimer(NULL, g_TimerId_LShift);
-	g_TimerId_LShift = 0;
-	phy_LShift_pressed = PHY_KEY_DOWN;
+	KillTimer(NULL, g_TimerId_Shift);
+	g_TimerId_Shift = 0;
+	phy_Shift_pressed = PHY_KEY_DOWN;
 }
 
-VOID CALLBACK TimerCallback_LAlt(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+VOID CALLBACK TimerCallback_Alt(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
 	(void)dwTime;
 	(void)idEvent;
 	(void)hwnd;
 	(void)uMsg;
 	// 停止定时器
-	KillTimer(NULL, g_TimerId_LAlt);
-	g_TimerId_LAlt = 0;
-	phy_LAlt_pressed = PHY_KEY_DOWN;
+	KillTimer(NULL, g_TimerId_Alt);
+	g_TimerId_Alt = 0;
+	phy_Alt_pressed = PHY_KEY_DOWN;
 }
 
 VOID CALLBACK TimerCallback_LWin(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
@@ -84,38 +95,39 @@ VOID CALLBACK TimerCallback_LWin(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD d
 	// 停止定时器
 	KillTimer(NULL, g_TimerId_LWin);
 	g_TimerId_LWin = 0;
-	phy_LAlt_pressed = PHY_KEY_DOWN;
+	phy_Alt_pressed = PHY_KEY_DOWN;
 }
 // ### 定时器回调函数 end
 
 // ### 多线程函数 begin
-DWORD WINAPI releaseLCtrlKeyState(LPVOID lpParam)
+DWORD WINAPI releaseCtrlKeyState(LPVOID lpParam)
 {
 	(void)lpParam;
 
-	volatile short count_lctrl = 0;
+	volatile short count_ctrl = 0;
 	while (1) {
-		if (phy_LCtrl_pressed == PHY_KEY_DOWN) {
+		if (phy_Ctrl_pressed == PHY_KEY_DOWN) {
 			// printf("phy_ctrl is down\n");
 			Sleep(50);
 			continue;
 		}
-		// 这里使用 count_lctrl 做次数控制, 是因为如果使用纯软件做按键模拟,
+		// 这里使用 count_ctrl 做次数控制, 是因为如果使用纯软件做按键模拟,
 		// 那么物理按键一定是 PHY_KEY_UP 状态 程序就要执行到这里 如果立即释放,
 		// 那么软件模拟的按键可能会失效, 比如软件模拟 ctrl+w 因为立即释放了 ctrl
 		// 所以实际输出就只是 w
-		while (KEY_DOWN(VK_LCONTROL)) { // lctrl
-			count_lctrl++;
+		while (KEY_DOWN(VK_LCONTROL) || KEY_DOWN(VK_RCONTROL)) { // ctrl
+			count_ctrl++;
 			Sleep(5);
-			if (count_lctrl == 10) {
+			if (count_ctrl == 10) {
 				break;
 			}
 		}
-		if (count_lctrl == 10) {
+		if (count_ctrl == 10) {
 			// std::cout << "lctrl will releaseL" << std::endl;
 			keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_RCONTROL, 0, KEYEVENTF_KEYUP, 0);
 		}
-		count_lctrl = 0;
+		count_ctrl = 0;
 		Sleep(50);
 	}
 }
@@ -124,62 +136,64 @@ DWORD WINAPI releaseLShiftKeyState(LPVOID lpParam)
 {
 	(void)lpParam;
 
-	volatile short count_lshift = 0;
+	volatile short count_shift = 0;
 	while (1) {
-		if (phy_LShift_pressed == PHY_KEY_DOWN) {
+		if (phy_Shift_pressed == PHY_KEY_DOWN) {
 			// printf("phy_lshift is down\n");
 			Sleep(50);
 			continue;
 		}
 
-		// 这里使用 count_lshift 做次数控制, 是因为如果使用纯软件做按键模拟,
+		// 这里使用 count_shift 做次数控制, 是因为如果使用纯软件做按键模拟,
 		// 那么物理按键一定是 PHY_KEY_UP 状态 程序就要执行到这里 如果立即释放,
 		// 那么软件模拟的按键可能会失效, 比如软件模拟 shift+f4 因为立即释放了 shift
 		// 所以实际输出就只是 f4
-		while (KEY_DOWN(VK_LSHIFT)) { // lshift
-			count_lshift++;
+		while (KEY_DOWN(VK_LSHIFT) || KEY_DOWN(VK_RSHIFT)) { // shift
+			count_shift++;
 			Sleep(5);
-			if (count_lshift == 10) {
+			if (count_shift == 10) {
 				break;
 			}
 		}
-		if (count_lshift == 10) {
+		if (count_shift == 10) {
 			// std::cout << "lshift will releaseL" << std::endl;
 			keybd_event(VK_LSHIFT, 0, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_RSHIFT, 0, KEYEVENTF_KEYUP, 0);
 		}
-		count_lshift = 0;
+		count_shift = 0;
 		Sleep(50);
 	}
 }
 
-DWORD WINAPI releaseLAltKeyState(LPVOID lpParam)
+DWORD WINAPI releaseAltKeyState(LPVOID lpParam)
 {
 	(void)lpParam;
 
-	volatile short count_lalt = 0;
+	volatile short count_alt = 0;
 	while (1) {
-		if (phy_LAlt_pressed == PHY_KEY_DOWN) {
+		if (phy_Alt_pressed == PHY_KEY_DOWN) {
 			// printf("phy_alt is down\n");
 			Sleep(50);
 			continue;
 		}
 
-		// 这里使用 count_lalt 做次数控制, 是因为如果使用纯软件做按键模拟,
+		// 这里使用 count_alt 做次数控制, 是因为如果使用纯软件做按键模拟,
 		// 那么物理按键一定是 PHY_KEY_UP 状态 程序就要执行到这里 如果立即释放,
 		// 那么软件模拟的按键可能会失效, 比如软件模拟 alt+f4 因为立即释放了 alt
 		// 所以实际输出就只是 f4
-		while (KEY_DOWN(VK_LMENU)) { // lalt
-			count_lalt++;
+		while (KEY_DOWN(VK_LMENU) || KEY_DOWN(VK_RMENU)) { // alt
+			count_alt++;
 			Sleep(5);
-			if (count_lalt == 10) {
+			if (count_alt == 10) {
 				break;
 			}
 		}
-		if (count_lalt == 10) {
+		if (count_alt == 10) {
 			// std::cout << "alt will releaseL" << std::endl;
 			keybd_event(VK_LMENU, 0, KEYEVENTF_KEYUP, 0);
+			keybd_event(VK_RMENU, 0, KEYEVENTF_KEYUP, 0);
 		}
-		count_lalt = 0;
+		count_alt = 0;
 		Sleep(50);
 	}
 }
@@ -240,31 +254,32 @@ DWORD WINAPI CheckPhyKeyState(LPVOID lpParam)
 	while (interception_receive(context, device = interception_wait(context), &stroke, 1) > 0) {
 		if (interception_is_keyboard(device)) {
 			InterceptionKeyStroke &keystroke = *(InterceptionKeyStroke *)&stroke;
-			if (keystroke.code == SCANCODE_LALT) {
-				phy_LAlt_pressed = keystroke.state;
+			if (keystroke.code == SCANCODE_LALT || keystroke.code == SCANCODE_RALT) {
+				phy_Alt_pressed = keystroke.state;
 				// std::cout << "lalt state: " << keystroke.state << std::endl;
 				// keybd_event(VK_LMENU, 0, KEYEVENTF_KEYUP, 0);
 				// 启动定时器
-				if (g_TimerId_LAlt != 0) {
-					KillTimer(NULL, g_TimerId_LAlt);
+				if (g_TimerId_Alt != 0) {
+					KillTimer(NULL, g_TimerId_Alt);
 				}
-				g_TimerId_LAlt = SetTimer(NULL, 0, 500, TimerCallback_LAlt);
+				g_TimerId_Alt = SetTimer(NULL, 0, 500, TimerCallback_Alt);
 			}
-			if (keystroke.code == SCANCODE_LSHIFT) {
-				phy_LShift_pressed = keystroke.state;
-				if (g_TimerId_LShift != 0) {
-					KillTimer(NULL, g_TimerId_LShift);
+			if (keystroke.code == SCANCODE_LSHIFT ||
+			    keystroke.code == SCANCODE_RSHIFT) {
+				phy_Shift_pressed = keystroke.state;
+				if (g_TimerId_Shift != 0) {
+					KillTimer(NULL, g_TimerId_Shift);
 				}
-				g_TimerId_LShift = SetTimer(NULL, 0, 500, TimerCallback_LShift);
+				g_TimerId_Shift = SetTimer(NULL, 0, 500, TimerCallback_Shift);
 			}
-			if (keystroke.code == SCANCODE_LCTRL) {
-				phy_LCtrl_pressed = keystroke.state;
-				if (g_TimerId_LCtrl != 0) {
-					KillTimer(NULL, g_TimerId_LCtrl);
+			if (keystroke.code == SCANCODE_LCTRL || keystroke.code == SCANCODE_RCTRL) {
+				phy_Ctrl_pressed = keystroke.state;
+				if (g_TimerId_Ctrl != 0) {
+					KillTimer(NULL, g_TimerId_Ctrl);
 				}
-				g_TimerId_LCtrl = SetTimer(NULL, 0, 500, TimerCallback_LCtrl);
+				g_TimerId_Ctrl = SetTimer(NULL, 0, 500, TimerCallback_Ctrl);
 			}
-                        // 经过测试发现win的物理按键和虚拟按键的键码都一个样, 所以无法判断
+			// 经过测试发现win的物理按键和虚拟按键的键码都一个样, 所以无法判断
 			// if (keystroke.code == SCANCODE_LWIN) {
 			// 	phy_LWin_pressed = keystroke.state;
 			// 	if (g_TimerId_LWin != 0) {
@@ -295,18 +310,18 @@ int main()
 		printf("线程创建失败\n");
 		return 1;
 	}
-	g_ThreadHandle_LAlt = CreateThread(NULL, 0, releaseLAltKeyState, NULL, 0, NULL);
-	if (g_ThreadHandle_LAlt == NULL) {
+	g_ThreadHandle_Alt = CreateThread(NULL, 0, releaseAltKeyState, NULL, 0, NULL);
+	if (g_ThreadHandle_Alt == NULL) {
 		printf("线程创建失败\n");
 		return 1;
 	}
-	g_ThreadHandle_LCtrl = CreateThread(NULL, 0, releaseLCtrlKeyState, NULL, 0, NULL);
-	if (g_ThreadHandle_LCtrl == NULL) {
+	g_ThreadHandle_Ctrl = CreateThread(NULL, 0, releaseCtrlKeyState, NULL, 0, NULL);
+	if (g_ThreadHandle_Ctrl == NULL) {
 		printf("线程创建失败\n");
 		return 1;
 	}
-	g_ThreadHandle_LShift = CreateThread(NULL, 0, releaseLShiftKeyState, NULL, 0, NULL);
-	if (g_ThreadHandle_LShift == NULL) {
+	g_ThreadHandle_Shift = CreateThread(NULL, 0, releaseLShiftKeyState, NULL, 0, NULL);
+	if (g_ThreadHandle_Shift == NULL) {
 		printf("线程创建失败\n");
 		return 1;
 	}
@@ -321,16 +336,16 @@ int main()
 
 	// 等待线程退出
 	WaitForSingleObject(g_ThreadHandle, INFINITE);
-	WaitForSingleObject(g_ThreadHandle_LAlt, INFINITE);
-	WaitForSingleObject(g_ThreadHandle_LCtrl, INFINITE);
-	WaitForSingleObject(g_ThreadHandle_LShift, INFINITE);
+	WaitForSingleObject(g_ThreadHandle_Alt, INFINITE);
+	WaitForSingleObject(g_ThreadHandle_Ctrl, INFINITE);
+	WaitForSingleObject(g_ThreadHandle_Shift, INFINITE);
 	// WaitForSingleObject(g_ThreadHandle_LWin, INFINITE);
 
 	// 关闭线程句柄
 	CloseHandle(g_ThreadHandle);
-	CloseHandle(g_ThreadHandle_LAlt);
-	CloseHandle(g_ThreadHandle_LCtrl);
-	CloseHandle(g_ThreadHandle_LShift);
+	CloseHandle(g_ThreadHandle_Alt);
+	CloseHandle(g_ThreadHandle_Ctrl);
+	CloseHandle(g_ThreadHandle_Shift);
 	// CloseHandle(g_ThreadHandle_LWin);
 
 	return 0;
